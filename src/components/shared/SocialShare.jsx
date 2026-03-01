400dc69a0fc5422a83af54c4da89f27b89990cf0
Julien Pagin <paginjulien@gmail.com>
2026-03-01 22:58:51 +0000

    File changes

--- src/components/shared/SocialShare.jsx
+++ src/components/shared/SocialShare.jsx
@@ -15,10 +15,14 @@ export default function SocialShare({ title, description }) {
     setTimeout(() => setCopied(false), 2000);
   };
 
+  const TikTokIcon = () => (
+    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-2.88 2.5 2.89 2.89 0 0 1-2.89-2.89 2.89 2.89 0 0 1 2.89-2.89c.28 0 .54.04.79.1V9.01a6.33 6.33 0 0 0-.79-.05 6.34 6.34 0 0 0-6.34 6.34 6.34 6.34 0 0 0 6.34 6.34 6.34 6.34 0 0 0 6.33-6.34V8.69a8.18 8.18 0 0 0 4.78 1.52V6.75a4.85 4.85 0 0 1-1.01-.06z"/></svg>
+  );
+
   const links = [
-    { icon: Facebook, label: 'Facebook', href: `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(shareUrl)}&quote=${encodeURIComponent(shareText)}` },
-    { icon: Twitter, label: 'X / Twitter', href: `https://twitter.com/intent/tweet?text=${encodeURIComponent(shareText)}&url=${encodeURIComponent(shareUrl)}` },
-    { icon: Linkedin, label: 'LinkedIn', href: `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(shareUrl)}` },
+    { icon: Facebook, label: 'Facebook', href: `https://www.facebook.com/61575203516618/` },
+    { icon: Instagram, label: 'Instagram', href: `https://www.instagram.com/fashionist.art.dour/` },
+    { icon: TikTokIcon, label: 'TikTok', href: `https://www.tiktok.com/@user6921475292315?_r=1&_t=ZG-94Kw7dIEGcS` },
   ];
 
   return (
