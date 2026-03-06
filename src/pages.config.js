3dd31143ab0aa1d81ef7df58701e91ebd24a6e82
Julien Pagin <paginjulien@gmail.com>
2026-03-06 22:15:50 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -66,6 +66,7 @@ import Privacy from './pages/Privacy';
 import Programme from './pages/Programme';
 import Sitemap from './pages/Sitemap';
 import Unsubscribe from './pages/Unsubscribe';
+import ArtistDetail from './pages/ArtistDetail';
 import __Layout from './Layout.jsx';
 
 
@@ -89,6 +90,7 @@ export const PAGES = {
     "Programme": Programme,
     "Sitemap": Sitemap,
     "Unsubscribe": Unsubscribe,
+    "ArtistDetail": ArtistDetail,
 }
 
 export const pagesConfig = {
